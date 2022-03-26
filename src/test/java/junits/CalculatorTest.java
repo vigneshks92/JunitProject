3a7158e6d257aa664f77a5da31ecb1434afe6af8
Vignesh K S <vigneshpri026@gmail.com>
2022-03-26 12:51:17 +0530

    Junit Project First Commit

---
+++ src/test/java/junits/CalculatorTest.java
@@ -0,0 +1,23 @@
+package junits;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+	
+	@Test
+	public void addTest1() {
+		
+		assertEquals(9,Calculator.add(4, 5));
+		
+	}
+	
+	@Test
+	public void subTest1() {
+		
+		assertEquals(1,Calculator.sub(5, 4));
+	
+	}
+
+}
